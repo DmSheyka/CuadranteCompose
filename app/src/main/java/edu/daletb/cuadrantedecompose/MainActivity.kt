4d683b17525ffe35a0d481b066eb10f66ce9f41a
dmsheyka <dmsheyka@gmail.com>
2024-03-13 09:54:16 -0400

    Proyecto terminado

--- app/src/main/java/edu/daletb/cuadrantedecompose/MainActivity.kt
+++ app/src/main/java/edu/daletb/cuadrantedecompose/MainActivity.kt
@@ -3,17 +3,25 @@ package edu.daletb.cuadrantedecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import edu.daletb.cuadrantedecompose.ui.theme.CuadranteDeComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,24 +48,29 @@ fun AppCuadranteCompose() {
             TarjetaComposable(
                 title = stringResource(R.string.titulo_texto_composable),
                 description = stringResource(R.string.descripcion_texto_composable),
-                bakckground = Color(0xFFEADDFF)
+                background = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
             )
             TarjetaComposable(
                 title = stringResource(R.string.titulo_imagen_composable) ,
                 description = stringResource(R.string.descripcion_imagen_composable),
-                bakckground = Color(0xFFD0BCFF)
+                background = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
             )
+        }
+        Row(Modifier.weight(1f)){
             TarjetaComposable(
                 title = stringResource(R.string.titulo_fila_composable),
                 description = stringResource(R.string.descripcion_fila_composable) ,
-                bakckground = Color(0xFFB69DF8)
+                background = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
             )
             TarjetaComposable(
                 title = stringResource(R.string.titulo_columna_composable) ,
                 description = stringResource(R.string.descripcion_columna_composable),
-                bakckground = Color(0xFFF6EDFF)
+                background = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
             )
-
         }
     }
 }
@@ -66,11 +79,30 @@ fun AppCuadranteCompose() {
 fun TarjetaComposable(
     title : String,
     description : String,
-    bakckground : Color,
+    background : Color,
     modifier : Modifier = Modifier
 ){
-
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(background)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun VistaPreviaCuadranteCompose() {
